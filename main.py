b588f0c53f9fffa07608d7737e661cd005a8f1ad
kingofnetflix <72701796+kingofnetflix@users.noreply.github.com>
2024-04-30 18:14:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ from discord.ext import commands
 intents = discord.Intents.all()
 intents.messages = True
 bot = commands.Bot(command_prefix='!', intents=intents)
-// this system is really bad, i dont encourage using it like this but this temp is mainly for me lol
+# this system is really bad, i dont encourage using it like this but this temp is mainly for me lol
 
 @bot.event
 async def on_ready():
-    print(f'logged in as {bot.user.name} cuhhh')
+    print(f'logged in as {bot.user.name}')
 
 @bot.tree.command(name="example", description="example interaction / slash command")
-async def check_bots(interaction: discord.Interaction):
+async def example(interaction: discord.Interaction):
     await interaction.response.send_message('hello')
 
 
